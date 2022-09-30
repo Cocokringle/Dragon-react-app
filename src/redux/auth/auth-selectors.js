4866475b26600dd20bb41d0b00d448db5c20fc87
Nataliia Belodedenko <cocokringle.1@gmail.com>
2022-09-30 17:27:40 +0300

    Configures the display of the user name

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,12 +1,12 @@
 const getIsLoggedIn = state => state.auth.isLoggedIn;
 
-// const getUsername = state => state.auth.user.name;
+const getUsername = state => state.auth.user.name;
 
 const getIsFetchingCurrentUser = state => state.auth.isFetchingCurrentUser;
 
 const authSelectors = {
     getIsLoggedIn,
-    // getUsername,
+    getUsername,
     getIsFetchingCurrentUser,
 };
 export default authSelectors;
